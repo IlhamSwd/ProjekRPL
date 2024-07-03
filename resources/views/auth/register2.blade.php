11e67def32c41821ff5dd95e6f864347b8315d26
IlhamSwd <160827594+IlhamSwd@users.noreply.github.com>
2024-07-03 21:35:34 +0700

    push

--- resources/views/auth/register2.blade.php
+++ resources/views/auth/register2.blade.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="manifest" href="site.webmanifest">
     <link rel="shortcut icon" type="image/x-icon" href="{{ url('assets/img/favicon.ico') }}">
-
+    
     <!-- CSS here -->
     <link rel="stylesheet" href="{{ url('assets/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ url('assets/css/owl.carousel.min.css') }}">
